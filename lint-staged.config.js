44df64a44854c0cbc75edf7ceeb2db83ccdd8a99
Anurag Patil <aspanurag@gmail.com>
2020-08-26 15:32:16 -0400

    random: commit|testing husky

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,11 +1,6 @@
 module.exports = {
-  
-    
-  
-    linters: {
-    '**/*.+(js|md|ts|css|sass|less|graphql|yml|yaml|scss|json|vue)': [
-      'prettier --write',
-      'git add',
-    ],
-  },
+  '**/*.+(js|md|ts|css|sass|less|graphql|yml|yaml|scss|json)': [
+    'prettier --write',
+    'git add',
+  ],
 };
